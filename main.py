6eaed8fc1382e2211436ba0a4fed241114bedea8
Elliot <didiel.com@gmail.com>
2023-09-11 20:33:50 +0200

    z

--- main.py
+++
@@ -1,9 +0,0 @@
-# USING UTF-8
-
-from pyautogui import *
-import time,random,functions,datetime
-
-click(1280,800) # Clique sur l'écran de gauche
-functions.goToPokeCenter()
-while True:
-    functions.fishingCycle()
