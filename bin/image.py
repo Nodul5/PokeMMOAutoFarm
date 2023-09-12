9a04bf46c377b80646e43591fd35982f5f3a91e0
LeoDef <75366169+LeoDef@users.noreply.github.com>
2023-09-12 19:57:16 +0200

    Création de la classe Image

--- bin/image.py
+++ bin/image.py
@@ -4,34 +4,25 @@ import numpy as np
 ## CONSTANT
 CHAT_PADDING = 33
 
-# read image
-img = cv2.imread('./image/test.png')
-ht, wd = img.shape[:2]
-print(img.shape)
-
-img = img[int(ht/2):ht, 0:int(wd/2)]
-cv2.imwrite('./image/crop.png',img)
-
-## Couleur de fond : #222222
-BlueMin = np.array([34, 34, 34],np.uint8)
-BlueMax = np.array([34, 34, 34],np.uint8)
-mask = cv2.inRange(img, BlueMin, BlueMax)
-cv2.imwrite('./image/mask.png',mask)
-
-## Récupérer l'image masquée
-#masked = cv2.bitwise_and(img,img, mask=mask)
-#img = img - masked
-
-# invert
-#img = 255 - img
+class Image:
+    def __init__(self,image):
+        self.image = image
     
-# get largest external contour
-contours = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-contours = contours[0] if len(contours) == 2 else contours[1]
-big_contour = max(contours, key=cv2.contourArea)
+    def cropChat(self):
+        # On prend le quart bas gauche du screen
+        ht, wd = self.image.shape[:2]
+        self.image = self.image[int(ht/2):ht, 0:int(wd/2)]
 
-# get bounding box of largest contour
-x,y,w,h = cv2.boundingRect(big_contour)
+        ## Mask en fonction de la couleur de fond du chat : #222222
+        BlueMin = np.array([34, 34, 34],np.uint8)
+        BlueMax = np.array([34, 34, 34],np.uint8)
+        mask = cv2.inRange(self.image, BlueMin, BlueMax)
+            
+        # Récupération du contour du chat
+        contours = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours = contours[0] if len(contours) == 2 else contours[1]
+        big_contour = max(contours, key=cv2.contourArea)
+        x,y,w,h = cv2.boundingRect(big_contour)
 
-# save resulting image
-cv2.imwrite('./image/result.png',img[y:y+h-CHAT_PADDING, x:x+w])
+        self.cropped = self.image[y:y+h-CHAT_PADDING, x:x+w]
+        return self.cropped
